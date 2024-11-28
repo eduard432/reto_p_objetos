acf216896570cda5e5b740dbeac672fc057e83ed
Eduardo <juantokx@gmail.com>
2024-11-28 14:49:08 -0600

    Comentarios en Cliente y en main

--- Cliente.cpp
+++ Cliente.cpp
@@ -11,10 +11,12 @@ using namespace std;
 
 #include "Cliente.h"
 
+// Constructor por defecto
 Cliente::Cliente() {
     numBoletos=0;
 }
 
+// Constructor asignando variables
 Cliente::Cliente(string nombre, string correo, string contrasenia) {
     numBoletos=0;
     this->nombre = nombre;
@@ -22,8 +24,10 @@ Cliente::Cliente(string nombre, string correo, string contrasenia) {
     this->contrasenia = contrasenia;
 }
 
+// Destructor por defecto
 Cliente::~Cliente() {}
 
+// Getters
 string Cliente::getNombre() {
     return nombre;
 }
@@ -36,6 +40,7 @@ string Cliente::getContrasenia() {
     return contrasenia;
 }
 
+// Setters
 void Cliente::setNombre(string nombre) {
     this->nombre = nombre;
 }
@@ -48,10 +53,12 @@ void Cliente::setContrasenia(string contrasenia) {
     this->contrasenia = contrasenia;
 }
 
+// Método para inicar sesión verificando si el correo y contraseña son iguale
 bool Cliente::iniciarSesion(string correo, string contrasenia) {
     return (this->correo == correo && this->contrasenia == contrasenia);
 }
 
+// Verificar si previamente ya fue registrado el correo
 bool Cliente::verificarRegistro(Cliente clientes[], int numClientes) {
     for (int i = 0; i < numClientes; i++) {
         if (clientes[i].getCorreo() == correo) {
@@ -61,11 +68,18 @@ bool Cliente::verificarRegistro(Cliente clientes[], int numClientes) {
     return true;
 }
 
+// Método para agregar el boleto al registro de clientes
+// Devuelve un booleano dependiendo si el boleto pudo ser agregado o no
 bool Cliente::agregarBoleto(Boleto boletoRecienComprado) {
+    // Obtenemos el concierot a partir del Boleto recibido
     Concierto concierto = boletoRecienComprado.getConcierto();
+    // Obtenemos datos del concierto
     int cantidadConcierto = concierto.getLimiteCompra();
     int cantidad = boletoRecienComprado.getCantidad();
+
+    // Verificamos si el usuario no ha excedido su máximo de boletos
     if (numBoletos < MAX_BOLETOS) {
+        // Verificamos si la cantidad de compra es menor a la compra máxima del concierto
         if(cantidad <= cantidadConcierto) {
             boletosComprados[numBoletos] = boletoRecienComprado;
             numBoletos++;
@@ -81,12 +95,15 @@ bool Cliente::agregarBoleto(Boleto boletoRecienComprado) {
     return false;
 }
 
+// Método para mostrar los boletos comprados.
 void Cliente::mostrarBoletosComprados() {
+    // Verificamos si el cliente tiene boletos
     if (numBoletos == 0) {
         cout << "No has comprado boletos." << endl;
         return;
     }
     cout << "Tus boletos comprados son:" << endl;
+    // Recorremos cada boleto del cliente y lo mostramos
     for (int i = 0; i < numBoletos; i++) {
         boletosComprados[i].mostrarBoleto();
         cout << "------------------------------" << endl;
--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 #include "Inmueble.h"
 #include "Cliente.h"
 
-// Dummies de inmuebles
+// Se crean inmuebles dummies
 Inmueble inmueble1("Foro Sol", 20);
 Inmueble inmueble2("Estadio Azteca", 30);
 Inmueble inmueble3("Auditorio Nacional", 100);
@@ -21,7 +21,7 @@ Inmueble inmueble4("Teatro Metropólitan", 60);
 Inmueble inmueble5("Arena Ciudad de Mexico", 40);
 Inmueble inmueble6("Palacio de los Deportes", 30);
 
-// Dummies de conciertos
+// Se crean  conciertos dummies
 const int MAX_CONCIERTOS = 5;
 Concierto conciertos[MAX_CONCIERTOS] = {
     Concierto("Guts Tour", "Olivia Rodrigo", "Pop/Rock", "02/04/2025", "19:00", inmueble2, 6),
@@ -31,7 +31,7 @@ Concierto conciertos[MAX_CONCIERTOS] = {
     Concierto("Domin Tour", "Stray Kids", "K-Pop", "04/03/2025", "21:00", inmueble2, 5),
 };
 
-// Dummies de clientes
+// Se crean clientes dummies
 const int MAX_CLIENTES = 10;
 Cliente clientes[MAX_CLIENTES] = {
     Cliente("Juan", "juan@ejemplo.com", "123"),
@@ -41,8 +41,9 @@ Cliente clientes[MAX_CLIENTES] = {
     Cliente("Luis", "luis@ejemplo.com", "def")
 };
 
-int numClientes = 5;
+int numClientes = 5; // Número actual de clientes registrados
 
+// Menú de cliente para compras
 void menuCliente(Cliente cliente) {
     int opcionCliente;
     int opcionConcierto;
@@ -58,9 +59,11 @@ void menuCliente(Cliente cliente) {
         cout << "------------------------------" << endl;
         cin >> opcionCliente;
 
+        // Si el cliente elige comprar boletos
         if(opcionCliente == 1) {
             cout << "------------------------------" << endl;
             cout << "Lista de conciertos disponibles:" << endl;
+            // Muestra los conciertos disponibles
             for (int i = 0; i < MAX_CONCIERTOS; i++) {
                 cout << i + 1 << ". " <<endl;
                 conciertos[i].mostrarConcierto();
@@ -74,6 +77,7 @@ void menuCliente(Cliente cliente) {
             cout << "Selecciona la cantidad de boletos: ";
             cin >> cantidad;
 
+            // Verifica que la opción de concierto y la cantidad de boletos sean válidas
             if (opcionConcierto >= 1 && opcionConcierto <= MAX_CONCIERTOS && cantidad>=1) {
                 cout << "------------------------------" << endl;
                 cout << "Has seleccionado el concierto: " << endl;
@@ -86,10 +90,12 @@ void menuCliente(Cliente cliente) {
                 cout << "Opcion no valida" << endl;
             }
 
+        // Si el cliente elige ver sus boletos
         } else if(opcionCliente == 2) {
             cout << "------------------------------" << endl;
             cliente.mostrarBoletosComprados();
 
+        // Si el cliente decide salir
         } else if(opcionCliente == 3) {
             salir = true;
         } else {
@@ -99,6 +105,7 @@ void menuCliente(Cliente cliente) {
     }
 }
 
+// Menú de sesión donde el cliente puede iniciar sesión o registrarse
 Cliente menuSesion() {
     int opcionSesion;
     bool inicioSesion = false;
@@ -113,6 +120,7 @@ Cliente menuSesion() {
         cout << "------------------------------" << endl;
         cin >> opcionSesion;
 
+        // Si elige iniciar sesión, solicita correo y contraseña
         if(opcionSesion == 1) {
             cout << "------------------------------" << endl;
             cout << "Ingrese su correo: ";
@@ -120,14 +128,16 @@ Cliente menuSesion() {
             cout << "Ingrese su contrasenia: ";
             cin >> contrasenia;
 
+            // Verifica si los datos coinciden con algún cliente registrado
             for (int i = 0; i < numClientes; i++) {
                 if (clientes[i].iniciarSesion(correo, contrasenia)) {
                     inicioSesion = true;
-                    return clientes[i];
+                    return clientes[i]; // Devuelve el cliente si la sesión es exitosa
                 }
             }
             cout << "------------------------------" << endl;
             cout << "Datos incorrectos, intente de nuevo." << endl;
+        // Si elige registrarse, solicita los datos para crear una nueva cuenta
         } else if(opcionSesion == 2) {
             cout << "------------------------------" << endl;
             cout << "Ingrese su nombre: ";
@@ -139,9 +149,10 @@ Cliente menuSesion() {
 
             Cliente nuevoCliente(nombre, correo, contrasenia);
 
+            // Verifica si el correo ya está registrado
             if (nuevoCliente.verificarRegistro(clientes, numClientes)) {
                 clientes[numClientes] = nuevoCliente;
-                numClientes++;
+                numClientes++; // Incrementa el número de clientes registrados
                 cout << "------------------------------" << endl;
                 cout << "Se creo la cuenta" << endl;
             } else {
@@ -153,11 +164,12 @@ Cliente menuSesion() {
             cout << "Opcion no valida, intente de nuevo" << endl;
         }
     }
-    return Cliente();
+    return Cliente(); // Devuelve un cliente vacío si no se logró iniciar sesión ni registrar
 }
 
 int main() {
+    // Inicia el programa con la selección de sesión de un cliente
     Cliente cliente = menuSesion();
-    menuCliente(cliente);
+    menuCliente(cliente); // Muestra el menú del cliente después de iniciar sesión
     return 0;
 }
